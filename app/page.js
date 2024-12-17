4736542f1cc89d35572aef6a58fef6da18ffebe8
Mahmud hasan <mahmudtarak24@gmail.com>
2024-12-17 13:33:33 +0600

    fetch data from TMDB in home

--- app/page.js
+++ app/page.js
@@ -1,7 +1,23 @@
 import React from 'react'
+import Results from './Components/Results';
+const API_KEY = process.env.API_KEY
+export default async function Home({searchParam}) {
 
-export default function Home() {
+  const genre = searchParam || "fetchTrending"
+  const res = await fetch(`https://api.themoviedb.org/3${
+    genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`}?api_key=${API_KEY}&
+     language=en-US&page=1`
+  );
+  const data = await res.json()
+  if(!res.ok){
+    throw new Error("Field to fetch data")
+  }
+  const results = data.results
+  
   return (
-    <div>Home</div>
+    <div>
+      <Results results={results}/>
+    </div>
   )
-}
+};
+
