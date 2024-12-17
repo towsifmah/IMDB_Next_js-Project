cd738f1580cc4ed057bd15f128907047d616ce94
Mahmud hasan <mahmudtarak24@gmail.com>
2024-12-17 12:16:06 +0600

    create Navbar component

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./Components/Header";
 import Providers from "./Providers";
+import Navbar from "./Components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,6 +27,7 @@ export default function RootLayout({ children }) {
       >
         <Providers>
         <Header/>
+        <Navbar/>
         {children}
         </Providers>
       </body>
