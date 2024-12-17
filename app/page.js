ddd70f3e4511339115d9e6d287fd33e2908d8cd1
Mahmud hasan <mahmudtarak24@gmail.com>
2024-12-17 18:33:22 +0600

    add card component

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import React from "react";
 import Results from "./Components/Results";
 const API_KEY = process.env.API_KEY;
 export default async function Home({ searchParams }) {
-  const genre = searchParams.genre || "fetchTrending";
+  const genre = searchParams?.genre || "fetchTrending";
   const res = await fetch(
     `https://api.themoviedb.org/3${
       genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
